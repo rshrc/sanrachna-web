230faec8cbbc5a7db03213d778d37c086889e97c
pratikjain04 <pratikjain0411@gmail.com>
2020-05-04 19:39:58 +0530

    added APIs
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

---
+++ lib/providers/associate_provider.dart
@@ -0,0 +1,17 @@
+import 'package:sanrachna_web/models/associate_model.dart';
+
+class AssociateProvider {
+  static List<AssociateModel> labours = [];
+
+  static List<AssociateModel> getLaboursData() {
+
+
+    return labours;
+
+  }
+
+
+
+
+
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+class Constants {
+
+  static String server = "http://sanrachna.pythonanywhere.com/api/";
+
+  static String labourAssociateAPI = "";
+  static String prospectAPI = "$server + prospect/list/";
+  static String materialTilesAPI = "$server + database/material/tiles/";
+  static String serviceAPI = "$server + database/service/";
+  static String clientClienteleAPI = "$server + admin/clientele/client/";
+  static String vendorAssociateAPI = "$server + associate/vendor/";
+
+}
