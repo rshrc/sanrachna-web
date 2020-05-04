c43d9474fa8dcce37e9d77635573a02af405ea1e
pratikjain04 <pratikjain0411@gmail.com>
2020-05-04 23:31:30 +0530

    updated Material APIs
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -8,13 +8,22 @@ class Constants {
   static String vendorAssociateAPI = "$server/associate/vendor/";
 
   static String prospectAPI = "$server/prospect/list/";
-  static String materialTilesAPI = "$server/database/material/tiles/";
   static String serviceAPI = "$server/ddatabase/service/";
 
   /// Clientele APIs
   static String clientClienteleAPI = "$server/clientele/client/";
   static String leadClienteleAPI = "$server/clientele/lead/";
   static String prospectClienteleAPI = "$server/clientele/prospect/";
+
+  /// Material APIs
+  static String materialTilesAPI = "$server/database/material/tiles/";
+  static String materialPlumbingAPI = "$server/database/material/plumbing/";
+  static String materialElectricAPI = "$server/database/material/electric/";
+  static String materialPlyAPI = "$server/database/material/ply/";
+  static String materialPaintAPI = "$server/database/material/paint/";
+  static String materialCivilAPI = "$server/database/material/civil/";
+
+
 
 
 }
