d4a50d1270e82432c09f21964c7b73f559fb5c0d
pratikjain04 <pratikjain0411@gmail.com>
2020-05-04 22:37:25 +0530

    APIs fixed, my bad
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,20 +1,20 @@
 class Constants {
 
-  static String server = "http://sanrachna.pythonanywhere.com/api/";
+  static String server = "http://sanrachna.pythonanywhere.com/api";
 
   /// Associate APIs
-  static String labourAssociateAPI = "$server + associate/labour/";
-  static String supervisorAssociateAPI = "$server + associate/supervisor/";
-  static String vendorAssociateAPI = "$server + associate/vendor/";
+  static String labourAssociateAPI = "$server/associate/labour/";
+  static String supervisorAssociateAPI = "$server/associate/supervisor/";
+  static String vendorAssociateAPI = "$server/associate/vendor/";
 
-  static String prospectAPI = "$server + prospect/list/";
-  static String materialTilesAPI = "$server + database/material/tiles/";
-  static String serviceAPI = "$server + database/service/";
+  static String prospectAPI = "$server/prospect/list/";
+  static String materialTilesAPI = "$server/database/material/tiles/";
+  static String serviceAPI = "$server/ddatabase/service/";
 
   /// Clientele APIs
-  static String clientClienteleAPI = "$server + admin/clientele/client/";
-  static String leadClienteleAPI = "$server + admin/clientele/lead/";
-  static String prospectClienteleAPI = "$server + admin/clientele/prospect/";
+  static String clientClienteleAPI = "$server/clientele/client/";
+  static String leadClienteleAPI = "$server/clientele/lead/";
+  static String prospectClienteleAPI = "$server/clientele/prospect/";
 
 
 }
