41a53716c1308d26c563be18af8cdde1232112fb
pratikjain04 <pratikjain0411@gmail.com>
2020-05-04 19:43:12 +0530

    added all APIs
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,11 +2,19 @@ class Constants {
 
   static String server = "http://sanrachna.pythonanywhere.com/api/";
 
-  static String labourAssociateAPI = "";
+  /// Associate APIs
+  static String labourAssociateAPI = "$server + associate/labour/";
+  static String supervisorAssociateAPI = "$server + associate/supervisor/";
+  static String vendorAssociateAPI = "$server + associate/vendor/";
+
   static String prospectAPI = "$server + prospect/list/";
   static String materialTilesAPI = "$server + database/material/tiles/";
   static String serviceAPI = "$server + database/service/";
+
+  /// Clientele APIs
   static String clientClienteleAPI = "$server + admin/clientele/client/";
-  static String vendorAssociateAPI = "$server + associate/vendor/";
+  static String leadClienteleAPI = "$server + admin/clientele/lead/";
+  static String prospectClienteleAPI = "$server + admin/clientele/prospect/";
+
 
 }
