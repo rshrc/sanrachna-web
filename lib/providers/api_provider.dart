122d6af1e1e92f6a1f0211c51f4129716ae3a915
Rishi Banerjee <rishieric91@gmail.com>
2020-05-04 19:41:36 +0530

    Abstract example overriden

---
+++ lib/providers/api_provider.dart
@@ -0,0 +1,66 @@
+import 'package:sanrachna_web/models/associate_model.dart';
+import 'package:sanrachna_web/models/clientelle_model.dart';
+import 'package:sanrachna_web/models/material_model.dart';
+import 'package:sanrachna_web/models/prospect_model.dart';
+
+abstract class AssociateAPIProvider {
+  Future<List<AssociateModel>> getLabours();
+
+  Future<List<AssociateModel>> getSupervisors();
+
+  Future<List<AssociateModel>> getVendors();
+
+  Future<String> addLabour();
+
+  Future<String> addSupervisor();
+
+  Future<String> addVendors();
+
+  Future<String> deleteLabour(int id);
+
+  Future<String> deleteSupervisor(int id);
+
+  Future<String> deleteVendor(int id);
+}
+
+abstract class ClientaleAPIProvider {
+  Future<List<ClientelleModel>> getClients();
+
+  Future<List<ClientelleModel>> getLeads();
+
+  Future<List<ClientelleModel>> getProspects();
+
+  Future<String> addClient();
+
+  Future<String> addLead();
+
+  Future<String> addProspect();
+
+  Future<String> deleteClient(int id);
+
+  Future<String> deleteLead(int id);
+
+  Future<String> deleteProspect(int id);
+}
+
+abstract class DatabaseAPIProvider {
+  Future<List<MaterialModel>> getMaterials();
+
+  Future<String> getServices();
+
+  Future<String> addMaterial();
+
+  Future<String> addService();
+
+  Future<String> deleteMaterial();
+
+  Future<String> deleteService();
+}
+
+abstract class ProspectAPIProvider {
+  Future<List<ProspectModel>> getProspects();
+
+  Future<String> addProspect();
+
+  Future<String> deleteProspect();
+}
--- lib/providers/associate_provider.dart
+++ lib/providers/associate_provider.dart
@@ -1,17 +1,60 @@
+
 import 'package:sanrachna_web/models/associate_model.dart';
+import 'package:sanrachna_web/providers/api_provider.dart';
 
-class AssociateProvider {
-  static List<AssociateModel> labours = [];
-
-  static List<AssociateModel> getLaboursData() {
-
-
-    return labours;
-
+class AssociateProvider implements AssociateAPIProvider {
+  @override
+  Future<String> addLabour() {
+    // TODO: implement addLabour
+    throw UnimplementedError();
   }
 
+  @override
+  Future<String> addSupervisor() {
+    // TODO: implement addSupervisor
+    throw UnimplementedError();
+  }
 
+  @override
+  Future<String> addVendors() {
+    // TODO: implement addVendors
+    throw UnimplementedError();
+  }
 
+  @override
+  Future<String> deleteLabour(int id) {
+    // TODO: implement deleteLabour
+    throw UnimplementedError();
+  }
 
+  @override
+  Future<String> deleteSupervisor(int id) {
+    // TODO: implement deleteSupervisor
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> deleteVendor(int id) {
+    // TODO: implement deleteVendor
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<AssociateModel>> getLabours() {
+    // TODO: implement getLabours
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<AssociateModel>> getSupervisors() {
+    // TODO: implement getSupervisors
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<AssociateModel>> getVendors() {
+    // TODO: implement getVendors
+    throw UnimplementedError();
+  }
 
 }
